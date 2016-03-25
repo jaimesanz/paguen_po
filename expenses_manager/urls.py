caf30fbfeeff06df85d5ae528b55e6932c0f073a
Jaime Sanz <jimbosanz23@gmail.com>
2016-03-25 15:02:51 -0300

    Implemented login system using django-registration

--- expenses_manager/urls.py
+++ expenses_manager/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from . import views
@@ -7,9 +7,7 @@ from . import views
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^home/', views.home, name='home'),
-	url(r'^register/', views.register, name='register'),
-	url(r'^login/$', views.user_login, name='login'),
-	url(r'^logout/$', views.user_logout, name='logout'),
+	url(r'^accounts/', include('registration.backends.hmac.urls')),
 	url(r'^login_test/$', views.login_test, name='login_test'),
 	url(r'^$', views.home, name='home')
 ]
