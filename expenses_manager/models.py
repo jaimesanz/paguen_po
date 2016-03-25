f1fc7d0439238e7c24d8c034fa3f35930e75e6a5
Jaime Sanz <jimbosanz23@gmail.com>
2016-03-25 18:14:05 -0300

    Added ViviendaUsuario model

--- expenses_manager/models.py
+++ expenses_manager/models.py
@@ -9,3 +9,11 @@ class Vivienda(models.Model):
 	alias = models.CharField(max_length=200)
 	def __unicode__(self):
 		return self.alias
+
+class ViviendaUsuario(models.Model):
+	class Meta:
+		unique_together = (('vivienda', 'user'),)
+	vivienda = models.ForeignKey(Vivienda, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	def __unicode__(self):
+		return self.vivienda + "__" + self.user
