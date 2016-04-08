37c8ddce7c95ec1404b3a38b7808dfba525eb510
Jaime Sanz <jimbosanz23@gmail.com>
2016-04-08 17:37:16 -0300

    Added dummy tests folder and classes

--- expenses_manager/tests.py
+++ expenses_manager/tests.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+class DummyTest(TestCase):
+	def test_dummy(self):
+		pass
