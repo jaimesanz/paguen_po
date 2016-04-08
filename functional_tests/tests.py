ee260500fdb80e1f57d2fe2e7e0974bad1268691
Jaime Sanz <jimbosanz23@gmail.com>
2016-04-08 17:37:30 -0300

    Added dummy tests folder and classes

---
+++ functional_tests/tests.py
@@ -0,0 +1,15 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class DummyFirfoxTest(LiveServerTestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_dummy(self):
+		pass
