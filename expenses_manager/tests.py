fdfea4c4fbf04fb63d57cc62e8e1f5726a6665b0
Jaime Sanz <jimbosanz23@gmail.com>
2016-04-09 20:39:28 -0300

    removed random comment

--- expenses_manager/tests.py
+++ expenses_manager/tests.py
@@ -840,28 +840,3 @@ class ErrorPageTest(TestCase):
 	def test_basics_error_url(self):
 		test_the_basics_not_logged_in(self, "/error/", "general/error.html", error)
 		test_the_basics_logged_in(self, "/error/", "general/error.html", error)
-
-# class LiveViewTest(TestCase):
-
-# 	def test_displays_all_items_for_that_list(self):
-# 		correct_list = List.objects.create()
-# 		Item.objects.create(text="itemy 1", list=correct_list)
-# 		Item.objects.create(text="itemy 2", list=correct_list)
-# 		other_list = List.objects.create()
-# 		Item.objects.create(text="other list item 1", list=other_list)
-# 		Item.objects.create(text="other list item 2", list=other_list)
-
-# 		response = self.client.get("/lists/%d/" % (correct_list.id))
-
-# 		self.assertContains(response, "itemy 1")
-# 		self.assertContains(response, "itemy 2")
-# 		self.assertNotContains(response, "other list item 1")
-# 		self.assertNotContains(response, "other list item 2")
-
-# 	def test_passes_correct_list_to_template(self):
-# 		other_list = List.objects.create()
-# 		correct_list = List.objects.create()
-
-# 		response = self.client.get("/lists/%d/" % (correct_list.id))
-
-# 		self.assertEqual(response.context["list"], correct_list)
