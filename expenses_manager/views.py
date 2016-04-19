bece892dd0b1b7f1b557fe8242120de45a3291ec
Jaime Sanz <jimbosanz23@gmail.com>
2016-04-19 17:39:40 -0300

    Extracted helper functions in views.py to independent file

--- expenses_manager/views.py
+++ expenses_manager/views.py
@@ -9,35 +9,10 @@ from django.forms.models import model_to_dict
 import json
 from django.utils import timezone
 from django.contrib import messages
-
-
-# helper functions
-def get_next_year_month_pair(y, m):
-    next_year = y
-    next_month = m + 1
-    if next_month > 12:
-        next_month = 1
-        next_year += 1
-    return (next_year, next_month)
-
-
-def get_periods(y0, m0, y1, m1):
-    periods = []
-    y = y0
-    m = m0
-    while y < y1 or (y <= y1 and m <= m1):
-        periods.append((y, m))
-        y, m = get_next_year_month_pair(y, m)
-    return periods
-
-
-def is_valid_year_month_range(y0, m0, y1, m1):
-    return True
+from expenses_manager.helper_functions import *
 
 
 def home(request):
-    # locals() creates a dict() object with all the variables from the local
-    # scope. We are passing it to the template
     return render(request, 'general/home.html', locals())
 
 
