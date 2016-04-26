c5858e0de3e5b90dccc93e096f73fa851115ca4e
Jaime Sanz <jimbosanz23@gmail.com>
2016-04-26 19:49:55 -0300

    PyLint changes

--- expenses_manager/helper_functions.py
+++ expenses_manager/helper_functions.py
@@ -1,21 +1,21 @@
 from django.contrib import messages
 
 
-def get_next_year_month_pair(y, m):
+def get_next_year_month_pair(year, month):
     """
     Takes 2 integers representing a Year and a Month.
     Returns the pair of Integers representing the next month from that period.
     If the month overflows (>12) it adds another year.
     """
-    next_year = y
-    next_month = m + 1
+    next_year = year
+    next_month = month + 1
     if next_month > 12:
         next_month = 1
         next_year += 1
     return (next_year, next_month)
 
 
-def get_periods(y0, m0, y1, m1):
+def get_periods(year0, month0, year1, month1):
     """
     Takes 4 integers representing 2 periods. The first and second argument
     represent the starting period, and the 3rd and 4rth arguments represent
@@ -23,20 +23,20 @@ def get_periods(y0, m0, y1, m1):
     the starting and end periods, including them.
     """
     periods = []
-    y = y0
-    m = m0
-    while y < y1 or (y <= y1 and m <= m1):
-        periods.append((y, m))
-        y, m = get_next_year_month_pair(y, m)
+    year = year0
+    month = month0
+    while year < year1 or (year <= year1 and month <= month1):
+        periods.append((year, month))
+        year, month = get_next_year_month_pair(year, month)
     return periods
 
 
-def is_valid_year_month_range(y0, m0, y1, m1):
+def is_valid_year_month_range(year0, month0, year1, month1):
     """
-    Returns True only if the period corresponding to y1,m1 is a date that comes
-    after  the period corresponding to y0,m0.
+    Returns True only if the period corresponding to year1,month1 is a date that comes
+    after  the period corresponding to year0,month0.
     """
-    return (y0 < y1) or (y0 == y1 and m0 <= m1)
+    return (year0 < year1) or (year0 == year1 and month0 <= month1)
 
 
 def user_has_vivienda(request):
