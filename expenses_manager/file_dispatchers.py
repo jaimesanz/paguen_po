d961ad322c970941ff44a14a4e466d3ac4776328
Jaime Sanz <jimbosanz23@gmail.com>
2016-05-24 19:22:23 -0400

    refactored get_gastos_xls function

--- expenses_manager/file_dispatchers.py
+++ expenses_manager/file_dispatchers.py
@@ -3,33 +3,35 @@ import xlwt
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .helper_functions.files import write_gastos_to_xls_sheet
+from .helper_functions.views import user_has_vivienda
+from .helper_functions.custom_decorators import request_passes_test
 
 
 @login_required
+@request_passes_test(user_has_vivienda,
+                     login_url="error",
+                     redirect_field_name=None)
 def get_gastos_xls(request):
-    response = HttpResponse(content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename=gastos.xls'
-
+    # compute gastos
     vu = request.user.get_vu()
-    gastos_pendientes_list, gastos_pagados_list = vu.get_gastos_vivienda()
-    gastos_pendientes_confirmacion_list = \
+    gastos_pendientes, gastos_pagados = vu.get_gastos_vivienda()
+    gastos_pendientes_confirmacion = \
         vu.vivienda.get_pending_confirmation_gastos()
 
+    # create XLS file
     wb = xlwt.Workbook()
-
-    ws_pagados = wb.add_sheet('Gastos Pagados')
-    ws_pendientes_confirm = wb.add_sheet('Gastos Pendientes Confirmación')
-    ws_pendientes = wb.add_sheet('Gastos Pendientes')
-
     write_gastos_to_xls_sheet(
-        ws_pagados,
-        gastos_pagados_list)
+        wb.add_sheet('Gastos Pagados'),
+        gastos_pagados)
     write_gastos_to_xls_sheet(
-        ws_pendientes_confirm,
-        gastos_pendientes_confirmacion_list)
+        wb.add_sheet('Gastos Pendientes Confirmación'),
+        gastos_pendientes_confirmacion)
     write_gastos_to_xls_sheet(
-        ws_pendientes,
-        gastos_pendientes_list)
+        wb.add_sheet('Gastos Pendientes'),
+        gastos_pendientes)
 
+    # create response object with said file
+    response = HttpResponse(content_type='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=gastos.xls'
     wb.save(response)
     return response
