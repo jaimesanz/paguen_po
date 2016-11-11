04e6e7cfb2f56290164fa20981e9052680b2e384
jaimesanz <jimbosanz23@gmail.com>
2016-11-11 15:32:58 -0300

    adjusting settings file

--- paguen_po/config/settings.py
+++ paguen_po/config/settings.py
@@ -9,13 +9,29 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-
 import os
-from .settings_secret import *
+import json
+
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# JSON-based secrets module
+with open(os.path.join(BASE_DIR, 'config', 'secrets.json')) as f:
+	secrets = json.loads(f.read())
+
+
+# get the secret variables using this method
+def get_secret(setting, secrets=secrets):
+	"""Get the secret variable or return explicit exception."""
+	try:
+		return secrets[setting]
+	except KeyError:
+		error_msg = "Set the {0} variable in secrets file.".format(setting)
+		raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret('secret_key')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -25,59 +41,72 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'expenses_manager.apps.ExpensesManagerConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'django.contrib.humanize',
-    'jquery',
-    'jquery_ui',
-    'bootstrap3',
-    'crispy_forms',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django_extensions',
+	'django.contrib.humanize',
+	'expenses_manager',
+	'jquery',
+	'jquery_ui',
+	'bootstrap3',
+	'crispy_forms',
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project_root.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                "django.template.context_processors.media",
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [
+			os.path.join(BASE_DIR, 'templates')
+		],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+				"django.template.context_processors.media",
+			],
+		},
+	},
 ]
 
-WSGI_APPLICATION = 'project_root.wsgi.application'
-
+WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': get_secret("db_name"),
+		'USER': get_secret("db_user"),
+		'PASSWORD': get_secret("db_pass"),
+		'HOST': get_secret("db_host"),
+		'PORT': get_secret("db_port"),
+		'TEST': {
+			'NAME': 'mytestdatabase',
+		}
+	}
+}
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
@@ -85,18 +114,18 @@ WSGI_APPLICATION = 'project_root.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 LOGIN_URL = '/accounts/login/'
@@ -134,7 +163,11 @@ DATE_FORMAT = "%Y-%m-%d"
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static_root'))
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -142,3 +175,15 @@ MEDIA_URL = '/media/'
 NOSE_ARGS = ['--nocapture', '--nologcapture', ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# email configuration
+# values for Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+# gmail account info
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
+# DEFAULT_TO_EMAIL = 'to email'
