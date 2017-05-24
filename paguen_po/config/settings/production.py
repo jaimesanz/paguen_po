2c925a59f92960f923852cda51bee2f91679bf5f
Ubuntu <ubuntu@ip-172-31-28-13.ec2.internal>
2017-05-24 01:07:23 +0000

    Updated production settings.

--- paguen_po/config/settings/production.py
+++ paguen_po/config/settings/production.py
@@ -3,9 +3,22 @@ from .base import *
 
 ALLOWED_HOSTS = ["*"]
 
+DEBUG = False
+
 EMAIL_BACKEND = "django_ses.SESBackend"
 DEFAULT_FROM_EMAIL = get_secret("default_from_email")
 EMAIL_HOST = ''
 
 AWS_ACCESS_KEY_ID = get_secret("aws_access_key_id")
 AWS_SECRET_ACCESS_KEY = get_secret("aws_secret_access_key")
+
+X_FRAME_OPTIONS = "DENY"
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
