ebc0d53e2e093e6a9afac97fc9c8ac11147fce4d
jaimesanz <jimbosanz23@gmail.com>
2017-08-13 13:21:52 -0300

    Basic Vue instance with vue-router is working and compiling.

---
+++ paguen_po/static_src/core/components/Base.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text" v-model="asdf">
+        <p v-text="asdf"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "paguenpo",
+        data () {
+            return {
+                asdf: "holaaa"
+            };
+        }
+    }
+</script>
--- paguen_po/static_src/core/index.js
+++ paguen_po/static_src/core/index.js
@@ -1,3 +1,22 @@
-window.ready(function () {
-    console.log("hola!");
+/*jslint browser: true */
+/*global $, console, alert */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Base from './components/Base.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        component: Base
+    }
+];
+
+const router = new VueRouter({
+    routes  // short for `routes: routes`
 });
+
+new Vue({
+    'router': router
+}).$mount('#app');
