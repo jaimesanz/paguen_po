a77aedb069253a5498eddb87c388d8329f4298c9
Jaime Sanz <jimbosanz23@gmail.com>
2018-11-16 18:26:13 -0300

    Docker compose stuff

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM python:3.5
-
 ENV PYTHONUNBUFFERED 1
 
 RUN mkdir /code
 WORKDIR /code
-
-ADD . /code/
-
+ADD . /code
 RUN pip install -r requirements/dev.txt
--- paguen_po/config/settings/production.py
+++ paguen_po/config/settings/production.py
@@ -9,3 +9,4 @@ ALLOWED_HOSTS = ["*"]
 #
 # AWS_ACCESS_KEY_ID = get_secret("aws_access_key_id")
 # AWS_SECRET_ACCESS_KEY = get_secret("aws_secret_access_key")
+DEBUG = False
